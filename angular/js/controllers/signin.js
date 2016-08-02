a0c12e3f4fa89ac28290ab7b84b02cedd18f41f4
h0ly byte <admin@kbve.com>
2016-08-02 00:40:17 +0000

    Saving Local Updates to Repo - Ignore Commit

--- angular/js/controllers/signin.js
+++ angular/js/controllers/signin.js
@@ -8,8 +8,9 @@ app.controller('SigninFormController', ['$scope', '$http', '$state', function($s
     $scope.login = function() {
       $scope.authError = null;
       // Try to login
-      $http.post('api/login', {email: $scope.user.email, password: $scope.user.password})
+      $http.post('https://kbve.com/forum/api/login', {email: $scope.user.email, password: $scope.user.password})
       .then(function(response) {
+        console.log(response);
         if ( !response.data.user ) {
           $scope.authError = 'Email or Password not right';
         }else{
